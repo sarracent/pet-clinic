9b4382ed94238a99a4bec18040fd536581cb7ceb
damian.sarracent <damian.sarracent@sondeos.com.ar>
2020-04-03 23:16:02 -0300

    Implement Interfaces for services. Closes #5

---
+++ pet-clinic-data/src/main/java/com/sarracent/petclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package com.sarracent.petclinic.model;
+
+public class Vet extends Person {
+}
---
+++ pet-clinic-data/src/main/java/com/sarracent/petclinic/services/PetService.java
@@ -0,0 +1,14 @@
+package com.sarracent.petclinic.services;
+
+import com.sarracent.petclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+}
