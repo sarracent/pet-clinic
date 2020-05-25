699c9b01fdf62dd9264f7fba81eaf70c48895352
damian.sarracent <damian.sarracent@sondeos.com.ar>
2020-05-25 19:45:29 -0300

    Adding Spring Config. Closes #22

--- pet-clinic-data/src/main/java/com/sarracent/petclinic/services/map/PetServiceMap.java
+++ pet-clinic-data/src/main/java/com/sarracent/petclinic/services/map/PetServiceMap.java
@@ -2,9 +2,11 @@ package com.sarracent.petclinic.services.map;
 
 import com.sarracent.petclinic.model.Pet;
 import com.sarracent.petclinic.services.PetService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Service
 public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetService {
 
     @Override
--- pet-clinic-web/src/main/java/com/sarracent/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/sarracent/petclinic/bootstrap/DataLoader.java
@@ -4,8 +4,6 @@ import com.sarracent.petclinic.model.Owner;
 import com.sarracent.petclinic.model.Vet;
 import com.sarracent.petclinic.services.OwnerService;
 import com.sarracent.petclinic.services.VetService;
-import com.sarracent.petclinic.services.map.OwnerServiceMap;
-import com.sarracent.petclinic.services.map.VetServiceMap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -15,9 +13,9 @@ public class DataLoader implements CommandLineRunner {
     private final OwnerService ownerService;
     private final VetService vetService;
 
-    public DataLoader() {
-        ownerService = new OwnerServiceMap();
-        vetService = new VetServiceMap();
+    public DataLoader(OwnerService ownerService, VetService vetService) {
+        this.ownerService = ownerService;
+        this.vetService = vetService;
     }
 
     @Override
